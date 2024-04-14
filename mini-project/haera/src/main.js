bb9a8ecc16c33ab17aa86d14fdc146de07409955
devnabi <klkl0812@naver.com>
2024-04-14 14:25:54 +0900

    Haera 저장소로 모두 이동하여 삭제

--- mini-project/haera/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '@/router/index.js';
-
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-createApp(App).use(router).mount('#app')
